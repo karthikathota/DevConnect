a6b93cde57524aa6651e6c36349231cf7884820a
karthikathota <saikarthik.tech@gmail.com>
2025-02-04 14:29:12 +0530

    Connection request modified

--- src/routes/request.js
+++ src/routes/request.js
@@ -17,15 +17,42 @@ requestRouter.post(
       const fromUserId = req.user.id;
       const toUserId = req.params.toUserId;
       const status = req.params.status;
+      const allowedStatus = ["ignore", "interested"];
 
-      console.log(fromUserId);
-      console.log(toUserId);
+      const toUser = await User.findById({ _id: toUserId });
+
+      if (!toUser) {
+        res.status(400).send("User not found");
+      }
+
+      if (!allowedStatus.includes(status)) {
+        res.status(400).send("Status invalid");
+      }
+
+      // Checking if fromUserId is equal to toUserId
+
+      if (fromUserId === toUserId) {
+        res.status(400).send("Cannot send connection to yourslef");
+      }
+
+      // Checking if connection request is duplicate
+      const existingConnectionRequest = await ConnectionRequestModel.findOne({
+        $or: [
+          { fromUserId: fromUserId, toUserId: toUserId },
+          { fromUserId: toUserId, toUserId: fromUserId },
+        ],
+      });
+
+      if (existingConnectionRequest) {
+        res.status(400).send("Request already exists");
+      }
+
+      // Creating a connection request
       const connectionRequest = new ConnectionRequestModel({
         fromUserId,
         toUserId,
         status,
       });
-
       const data = await connectionRequest.save();
 
       res.json({
